2f2f9ee0f7b5be8cf84c6d73c606aada72aa6658
Andreasmarcel <Andreasmarcel@cf1c6662-552b-0410-adbd-93d71fd7b10a>
2007-04-05 00:22:00 +0000

    Cleaned up, commented.. try it

--- Casio9860.c
+++ Casio9860.c
@@ -3,9 +3,10 @@
  * from user space using libusb
  *
  * Copyright (C) 2007
- *     Manuel Naranjo (naranjo.manuel@gmail.com)
+ *	Manuel Naranjo (naranjo.manuel@gmail.com)
+ *	Andreas Bertheussen (andreasmarcel@gmail.com)
  * Copyright (C) 2004
- *     Greg Kroah-Hartman (greg@kroah.com)
+ *	Greg Kroah-Hartman (greg@kroah.com)
  *
  * This program is free software; you can
  * redistribute it and/or modify it under the terms
@@ -87,25 +88,20 @@ void debug(int input, char* array, int len){
 
 struct usb_device *device_init(void)
 {
-    struct usb_bus *usb_bus;
-    struct usb_device *dev;
-    usb_init();
-    usb_find_busses();
-    usb_find_devices();
-    for (usb_bus = usb_busses;
-         usb_bus;
-         usb_bus = usb_bus->next) {
-        for (dev = usb_bus->devices;
-             dev;
-             dev = dev->next) {
-            if ((dev->descriptor.idVendor
-                  == C9860_VENDOR_ID) &&
-                (dev->descriptor.idProduct
-                  == C9860_PRODUCT_ID))
-                return dev;
-        }
-    }    
-    return NULL;
+	struct usb_bus *usb_bus;
+	struct usb_device *dev;
+	usb_init();
+	usb_find_busses();
+	usb_find_devices();
+	for (usb_bus = usb_busses;
+		usb_bus;
+		usb_bus = usb_bus->next) {
+		for (dev = usb_bus->devices; dev; dev = dev->next) {
+			if ((dev->descriptor.idVendor == C9860_VENDOR_ID) && (dev->descriptor.idProduct == C9860_PRODUCT_ID))
+				return dev;
+		}
+	}    
+	return NULL;
 }
 
 /**
@@ -115,107 +111,91 @@ int init_9860(struct usb_dev_handle *usb_handle){
 	char* out;
 	int retval;
 	out = calloc(0x12, sizeof(char));
-        
-    retval = usb_control_msg(usb_handle, 0x80, 0x6, 0x100, 0, out, 0x12, 200);
-    debug(1, out, retval);
-           
-    if (retval < 0){
-    	fprintf(stderr,
-    		"Not able to send first message\n");
-    	return retval;
-    }
-           
-    out = (char *)realloc(out, (size_t) (0x29 * sizeof(char)));
-         
-    retval = usb_control_msg(usb_handle, 0x80, 0x6, 0x200, 0, out, 0x29, 250);
-    debug(1, out, retval);
-    
-    if (retval < 0){
-    	fprintf(stderr,
-    		"Not able to send second message\n");
-    	return retval;
-    }
- 
+	    
+	retval = usb_control_msg(usb_handle, 0x80, 0x6, 0x100, 0, out, 0x12, 200);
+	debug(1, out, retval);
+	       
+	if (retval < 0){
+		fprintf(stderr, "Not able to send first message\n");
+		return retval;
+	}
+	       
+	out = (char *)realloc(out, (size_t) (0x29 * sizeof(char)));
+	     
+	retval = usb_control_msg(usb_handle, 0x80, 0x6, 0x200, 0, out, 0x29, 250);
+	debug(1, out, retval);
+
+	if (retval < 0){
+		fprintf(stderr, "Not able to send second message\n");
+		return retval;
+	}
+
 	out = (char *)realloc(out, (size_t) (0x1 * sizeof(char)));
-    
-    retval = usb_control_msg(usb_handle, 0x41, 0x1, 0x0, 0, out, 0x0, 250);
-    debug(1, out, retval);
-    
-    if (retval < 0){
-    	fprintf(stderr,
-    		"Not able to send third message\n");
-    	return retval;
-    }
-    
-    return 0;
+
+	retval = usb_control_msg(usb_handle, 0x41, 0x1, 0x0, 0, out, 0x0, 250);
+	debug(1, out, retval);
+
+	if (retval < 0){
+		fprintf(stderr, "Not able to send third message\n");
+			fprintf(stderr, "Not able to send third message\n");
+		return retval;
+	}
+
+	return 0;
 }
 
 int main(int argc, char **argv){
 	int retval = 1;
-    char* out;
-    char* temp;
-    struct usb_device *usb_dev;
-    struct usb_dev_handle *usb_handle;
-    
-    usb_dev = device_init();
+	char* out;
+	char* temp;
+	struct usb_device *usb_dev;
+	struct usb_dev_handle *usb_handle;
+
+	usb_dev = device_init();
+
+	if (usb_dev == NULL) {
+		fprintf(stderr, "Device not found\n");        
+		goto exit;
+	}
+
+	usb_handle = usb_open(usb_dev);
+	if (usb_handle == NULL) {
+		fprintf(stderr, "Not able to claim the USB device\n");
+		goto exit_close;
+	}
+
+	retval = usb_claim_interface(usb_handle, 0);
+	if (retval < 0) {
+		fprintf(stderr, "Not able to claim USB Interface\n");
+		goto exit_unclaim;
+	}
+	       
+	retval = init_9860(usb_handle);
+
+	if (retval < 0){
+		fprintf(stderr, "Couldn't initilizate device\n");
+		goto exit_unclaim;
+	}
+
+	out = calloc(0x40, sizeof(char));
 	
-    if (usb_dev == NULL) {
-        fprintf(stderr, "Device not found\n");        
-        goto exit;
-    }
-    
-    usb_handle = usb_open(usb_dev);
-    if (usb_handle == NULL) {
-        fprintf(stderr,
-             "Not able to claim the USB device\n");
-        goto exit_close;
-    }
-    
-    retval = usb_claim_interface(usb_handle, 0);
-    if (retval < 0) {
-    	fprintf(stderr,
-    		"Not able to claim USB Interface\n");
-    	goto exit_unclaim;    	
-    }
-           
-    retval = init_9860(usb_handle);
-    
-    if (retval < 0){
-    	fprintf(stderr,
-    		"Couldn't initilizate device\n");
-    	goto exit_unclaim;
-    }
-    
-    out = calloc(0x40, sizeof(char));        
-	out[0] =  0x05; out[1]=0x30; out[2]=0x30; out[3]=0x30;
-	retval = sendPackage(usb_handle,out,4);
+	// Connection verification, start of communication
+	out[0] =0x05; out[1]=0x30; out[2]=0x30; out[3]=0x30; out[4]=0x37; out[5]=0x30;  
+	retval = sendPackage(usb_handle,out,6);
 	if (retval < 0)		
-    	goto exit_unclaim;    
-	
-	
-	out[0] =  0x37; out[1]=0x30;  
-  	retval = sendPackage(usb_handle,out,2);
-	if (retval < 0)
-    	goto exit_unclaim;    
-	
+		goto exit_unclaim;
 
 	retval = readPackage(usb_handle,out,6);
 	if (retval < 0)		
 		goto exit_unclaim;
 	
- 	
- 	out[0] = 0x01; out[1] = 0x30; out[2] = 0x31; out[3]=0x30; 	
- 	retval = sendPackage(usb_handle,out,4);
+ 	// Request device identification
+ 	out[0] = 0x01; out[1] = 0x30; out[2] = 0x31; out[3]=0x30; out[4] = 0x36; out[5] = 0x46;
+ 	retval = sendPackage(usb_handle,out,6);
 	if (retval < 0)			
 		goto exit_unclaim;
-	
-	
-	out[0] = 0x36; out[1] = 0x46; 	
- 	retval = sendPackage(usb_handle,out,2);	
-	if (retval < 0)				
-		goto exit_unclaim;
-		
-	usleep(100*1000);
+
+	usleep(50*1000);
 	
 	temp = malloc (sizeof(char) * 0x40 * 3);
 	
@@ -225,13 +205,13 @@ int main(int argc, char **argv){
  		
  	memcpy(temp, out, retval);
  	 	
- 	usleep(100*1000);	
+ 	usleep(50*1000);	
  	retval = readPackage(usb_handle,out,0x40);
  	if (retval < 0)
  		goto exit_unclaim;
  	
  	memcpy(temp + 0x40, out, 0x40);
- 	usleep(100*1000);
+ 	usleep(50*1000);
  	retval = readPackage(usb_handle,out,0x2e);
  	if (retval < 0)
  		goto exit_unclaim;
@@ -239,14 +219,9 @@ int main(int argc, char **argv){
 	memcpy(temp + 0x80, out, retval);
 	parseHeader(temp, 0x80 + retval);
 	
-
-	out[0] = 0x01; out[1] = 0x33; out[2] = 0x33; out[3] = 0x30;
-	retval = sendPackage(usb_handle,out, 4); 		
-	if (retval < 0)
-		goto exit_unclaim;
-		
-	out[0] = 0x36; out[1] = 0x41;
-	retval = sendPackage(usb_handle,out, 2);
+	// Request all settings
+	out[0] = 0x01; out[1] = 0x33; out[2] = 0x33; out[3] = 0x30; out[4] = 0x36; out[5] = 0x41;
+	retval = sendPackage(usb_handle,out, 6); 		
 	if (retval < 0)
 		goto exit_unclaim;
 
@@ -254,87 +229,60 @@ int main(int argc, char **argv){
 	if (retval < 0)
 		goto exit_unclaim;
 
-
-
-	out[0]=0x03; out[1]=0x30; out[2]=0x30; out[3]=0x30;
-	retval = sendPackage(usb_handle,out,4);
+	// Change leadership to calculator
+	out[0]=0x03; out[1]=0x30; out[2]=0x30; out[3]=0x30; out[4]=0x37; out[5]=0x30;
+	retval = sendPackage(usb_handle,out,6);
 	if (retval < 0)
 		goto exit_unclaim;
-	 
-	 out[0]=0x37; out[2]=0x30;
-	 retval = sendPackage(usb_handle,out,2);
-	 if (retval < 0)
-	 	goto exit_unclaim;
-	 	
-get_list: 
-	 retval = readBuffer(usb_handle, out);
-	 if (retval < 0)
-	 	goto exit_unclaim;
-	 	
-	 if (out[0] == 0x1) {
-	 	out[0] = 0x06; out[1]=0x30; out[2]=0x30; out[3]=0x30;
-	 	retval = sendPackage(usb_handle,out, 4);
-	 	if (retval < 0)
-	 		goto exit_unclaim;
-	 		
-	 	out[0] = 0x37; out[1]=0x30;
-	 	retval = sendPackage(usb_handle,out, 2);
-	 	if (retval < 0)
-	 		goto exit_unclaim;
-	 	
-	 	 
-	 	usleep(200*1000);
-	 	goto get_list; 
-	 }
-	 
-	 out[0]=0x01; out[1]=0x32; out[2]=0x39; out[3]=0x30;
-	 retval = sendPackage(usb_handle,out, 4);
-	 if (retval < 0)
-	 	goto exit_unclaim;
-	 	
-	 out[0]=0x36; out[1]=0x35;
-	 retval = sendPackage(usb_handle,out, 2);
-	 if (retval < 0)
-	 	goto exit_unclaim;
-	 	
-	 retval = readPackage(usb_handle,out, 6);
-	 if (retval < 0)
-	 	goto exit_unclaim;
-	 	
-	 	
+	
+get_settings: 
+	retval = readBuffer(usb_handle, out);
+	if (retval < 0)
+		goto exit_unclaim;
+		
+	if (out[0] == 0x01) {	// if incoming is data, ack it.
+		out[0] = 0x06; out[1]=0x30; out[2]=0x30; out[3]=0x30; out[4] = 0x37; out[5]=0x30;
+		retval = sendPackage(usb_handle,out, 6);
+		if (retval < 0)
+			goto exit_unclaim;
+		 
+		usleep(100*1000);
+		goto get_settings; 
+	}
+	
+	// Get RAM image
+	out[0]=0x01; out[1]=0x32; out[2]=0x39; out[3]=0x30; out[4]=0x36; out[5]=0x35;
+	retval = sendPackage(usb_handle,out, 6);
+	if (retval < 0)
+		goto exit_unclaim;
 
-	 out[0]=0x03; out[1]=0x30; out[2]=0x30; out[3]=0x30;
-	 retval = sendPackage(usb_handle,out, 4);
-	 if (retval < 0)
-	 	goto exit_unclaim;
-	 	
-	 out[0]=0x37; out[1]=0x30;
-	 retval = sendPackage(usb_handle,out, 2);
-	 if (retval < 0)
-	 	goto exit_unclaim;
+	retval = readPackage(usb_handle,out, 6);
+	if (retval < 0)
+		goto exit_unclaim;
+		
+	// let calc take control
+	out[0]=0x03; out[1]=0x30; out[2]=0x30; out[3]=0x30; out[4]=0x37; out[5]=0x30;
+	retval = sendPackage(usb_handle,out, 4);
+	if (retval < 0)
+		goto exit_unclaim;
 
-get_list2:
-	 retval = 1;
-	 while(retval > 0) 	
-	 	retval = readBuffer(usb_handle, out);
-	 	
-	 if (retval < 0)
-	 	goto exit_unclaim;
-	 if (out[0]!= 0x1){
-	 	out[0]=0x06; out[1]=0x30; out[2]=0x30; out[3]=0x30;
-	 	retval = sendPackage(usb_handle,out, 4);
-	 	if (retval < 0)
-	 		goto exit_unclaim;
-	 		
-	 	out[0]=0x37; out[1]=0x30;
-	 	retval = sendPackage(usb_handle,out, 2);
-	 	if (retval < 0)
-	 		goto exit_unclaim;
-	 	
-	 	//usleep(100*1000);
-	 	goto get_list2;
-	 }
-	 	   
+get_ram:
+	retval = 1;
+	while(retval > 0) 	
+		retval = readBuffer(usb_handle, out);
+		
+	if (retval < 0 || out[0]==0x03)	// goes on until 'direction change' packet is received
+		goto exit_unclaim;
+	
+	out[0]=0x06; out[1]=0x30; out[2]=0x30; out[3]=0x30; out[4]=0x37; out[5]=0x30;
+	retval = sendPackage(usb_handle,out, 6);
+	if (retval < 0)
+		goto exit_unclaim;
+	
+	//usleep(100*1000);
+	goto get_ram;
+	
+
 exit_unclaim:
 	usb_release_interface(usb_handle, 0);
 exit_close:	
